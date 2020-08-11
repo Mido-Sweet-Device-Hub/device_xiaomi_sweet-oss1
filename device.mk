2d5c9bc77ee9d2c8b3b74b650b7cf426874bd574
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    sweet: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -119,4 +119,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+PRODUCT_PACKAGES += \
+    libshim_megvii
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
