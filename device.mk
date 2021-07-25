7302f112b44e701d6c0b3dce808e4c39710d65a3
daniml3 <danimoral1001@gmail.com>
2021-07-25 23:24:40 +0200

    sweet: show a ring on the camera cutout when its in use
    * For avoiding halo effect on low light conditions
    
    * Achieve this by creating a custom ImageView that its shown when the
      camera is in use
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Iedca0f0d4b304b020daf0ebec74e245581824c98

--- device.mk
+++ device.mk
@@ -203,6 +203,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_megvii
 
+PRODUCT_PACKAGES += \
+    CutoutRingServiceSweet
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-impl \
