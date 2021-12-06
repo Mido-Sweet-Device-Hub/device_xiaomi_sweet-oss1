a4fb43c21903f368da1d78ddee2c137f6c867936
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-06 18:46:11 +0000

    sweet: overlay: use stock build time overlays instead of RROs
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -114,13 +114,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
-PRODUCT_PACKAGES += \
-    CarrierConfigOverlaySweet \
-    DialerOverlaySweet \
-    FrameworkResOverlaySweet \
-    SettingsProviderOverlaySweet \
-    SystemUIOverlaySweet \
-    TelephonyOverlaySweet
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
