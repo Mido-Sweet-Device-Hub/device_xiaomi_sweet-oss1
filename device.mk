cc82f4ba626d67a2696e345d9ffb297fa4351b02
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-06 18:46:11 +0000

    sweet: overlay: use stock build time overlays instead of RROs
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -118,13 +118,8 @@ PRODUCT_PACKAGES += \
     XiaomiParts
 
 # Overlays
-PRODUCT_PACKAGES += \
-    CarrierConfigOverlaySweet \
-    DialerOverlaySweet \
-    FrameworkResOverlaySweet \
-    SettingsProviderOverlaySweet \
-    SystemUIOverlaySweet \
-    TelephonyOverlaySweet
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
