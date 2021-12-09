7916520958a49420c082bf7732bde0aac7b57819
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-09 19:54:29 +0000

    sweet: fix KERNEL_CMDLINE
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,15 +31,13 @@ TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
-                        androidboot.memcg=1 \
-                        lpm_levels.sleep_disabled=1 \
-                        video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 \
-                        service_locator.enable=1 \
-                        swiotlb=1 \
-                        androidboot.usbcontroller=a600000.dwc3
-
 BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_CMDLINE     := androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE     += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE     += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE     += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE     += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE     += service_locator.enable=1 swiotlb=1
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_KERNEL_SEPARATED_DTBO := true
