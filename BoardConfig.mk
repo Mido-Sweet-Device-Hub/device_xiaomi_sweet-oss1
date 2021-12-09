d11d88cfb9de44304c63105dd5dd3f9b47503224
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-09 20:07:20 +0000

    sweet: switch to vanton kernel
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,26 +32,32 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_CMDLINE     := androidboot.hardware=qcom
-BOARD_KERNEL_CMDLINE     += androidboot.memcg=1
-BOARD_KERNEL_CMDLINE     += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE     += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE     += androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE     += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE     += service_locator.enable=1 swiotlb=1
+BOARD_KERNEL_CMDLINE     += service_locator.enable=1
+BOARD_KERNEL_CMDLINE     += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE     += loop.max_part=7
+BOARD_KERNEL_CMDLINE     += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CONFIG := sweet_user_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+TARGET_KERNEL_CONFIG := sweet_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_ARCH := arm64
+
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+HOSTCFLAGS="-Wno-unused-command-line-argument"
+
+TARGET_KERNEL_APPEND_DTB := true
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 9126805504
