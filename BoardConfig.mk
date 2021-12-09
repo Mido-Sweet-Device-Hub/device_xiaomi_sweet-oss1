e343e338ed7d3bdd3cadf3f631d1fed675b16129
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-09 19:46:25 +0000

    sweet: switch to Enforcing
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
                         service_locator.enable=1 \
                         swiotlb=1 \
                         androidboot.usbcontroller=a600000.dwc3 \
-                        androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
