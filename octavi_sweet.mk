879126c3df9c222b4d89048192a3cec97292af4e
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-01-18 15:00:13 +0000

    sweet: update build fp & desc with Raven (Pixel 6 Pro) Jan patch
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- octavi_sweet.mk
+++ octavi_sweet.mk
@@ -31,6 +31,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
